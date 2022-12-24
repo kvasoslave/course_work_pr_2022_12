42633326373c71a98fd3bf67cec9cf16b434005e
kvasoslave <91786811+kvasoslave@users.noreply.github.com>
2022-12-24 23:29:38 +0300

    Update texttools.c

--- Курсач/texttools.c
+++ Курсач/texttools.c
@@ -1,5 +1,23 @@
 #include "texttools.h"
 
+int wcscasecmp(const wchar_t* s1, const wchar_t* s2)
+{
+	wchar_t c1, c2;
+
+	if (s1 == s2)
+		return 0;
+
+	do
+	{
+		c1 = towlower(*s1++);
+		c2 = towlower(*s2++);
+		if (c1 == L'\0')
+			break;
+	} while (c1 == c2);
+
+	return c1 - c2;
+}
+
 void init_Text(Text* text)
 {
 	text->sentences = NULL;
